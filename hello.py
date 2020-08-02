975927d54daebdfbb6a4f1f3a897075f03dfc69c
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-08-02 16:42:24 -0700

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+print('Enter a name: ')
+name = input()
+if name:
+    print('Thank you!')
+else:
+    print("You didn't enter a name.")
+
+
+ # testing
+
+#The lack of an input is a "falsey" value
